ecfb2b4875642acdfbce3dcbc5c488c317121162
Xpy010402 <2315323134>
2021-12-20 20:17:54 +0800

    第一次提交

---
+++ hello-vue/src/user/list.js
@@ -0,0 +1,13 @@
+const ListForm = {
+    template:'\
+    <div>\
+    <h2>列表页</h2> \
+    <table border="1" cellspacing="0" cellpadding="0" >\
+    <tr><th>年龄</th><th>姓名</th></tr>\
+    <tr><td>18</td><td>李白</td></tr>\
+    <tr><td>19</td><td>杜甫</td></tr>\
+    <tr><td>20</td><td>王维</td></tr>\
+    </table>\
+    </div>\
+    '
+}
